0f627eeb0147c288fac51a80a52ee9a5368d14af
Che-Misty <fash1210@gmail.com>
2026-06-22 01:33:18 +0300

    update main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
 var uID int
@@ -11,29 +10,14 @@ type User struct {
 	ID      int
 	Name    string
 	Balance float32
-	mu      sync.Mutex
-}
-
-func (u *User) NewUser(name string) *User {
-	u.mu.Lock()
-	uID += 1
-	u.mu.Unlock()
-	var balance float32
-
-	return &User{ID: uID, Name: name, Balance: balance}
 }
 
 func (u *User) Deposit(amount float32) {
-	u.mu.Lock()
-	defer u.mu.Unlock()
 	u.Balance += amount
 	fmt.Printf("Баланс пользователя %s равен: %.2f рублей.\n", u.Name, u.Balance)
 }
 
 func (u *User) Withdraw(amount float32) {
-	u.mu.Lock()
-	defer u.mu.Unlock()
-
 	if amount > u.Balance {
 		fmt.Printf("На счету пользователя %s недостаточно средств для списания %.2f рублей.\n", u.Name, u.Balance)
 		return
@@ -43,15 +27,16 @@ func (u *User) Withdraw(amount float32) {
 }
 
 func main() {
-	var u User
-	accounts := make(map[string]*User)
-	username := []string{"Kate", "Dung", "Son", "Ngoc"}
-	for _, val := range username {
-		v := val
-		accounts[v] = u.NewUser(v)
-	}
+	user1 := &User{1, "Kate", 100}
+	user2 := &User{2, "Dung", 100}
 
-	for _, usr := range accounts {
-		fmt.Printf("id=%d name=%s balance=%.2f\n", usr.ID, usr.Name, usr.Balance)
-	}
+	user2.Withdraw(150)
+	user1.Deposit(200)
+	user1.Withdraw(135)
+	user2.Deposit(200)
+	user1.Withdraw(237)
+	user2.Deposit(1200)
+	user2.Withdraw(400)
+	user1.Deposit(22)
+	user2.Withdraw(1000)
 }
