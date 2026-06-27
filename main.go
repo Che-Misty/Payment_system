89839fcb9844d19f3f5e6fddb78d9eb922a8a99f
Che-Misty <fash1210@gmail.com>
2026-06-27 15:50:42 +0300

    Исправление кода

--- main.go
+++ main.go
@@ -2,48 +2,42 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"sync"
 )
 
-var uID int
-
 type User struct {
-	ID      int
+	ID      string
 	Name    string
 	Balance float64
 	mu      sync.Mutex
 }
 
-func NewUser(name string, balance float64) *User {
-	uID += 1
-
-	return &User{ID: uID, Name: name, Balance: balance}
+func NewUser(id string, name string, balance float64) *User {
+	return &User{ID: id, Name: name, Balance: balance}
 }
 
 type Transaction struct {
-	FromID int
-	ToID   int
+	FromID string
+	ToID   string
 	Amount float64
 }
 
 type PaymentSystem struct {
-	Users        map[int]*User
+	Users        map[string]*User
 	Transactions []Transaction
 	mu           sync.Mutex
 }
 
 func NewPaymentSystem() *PaymentSystem {
-	return &PaymentSystem{Users: make(map[int]*User), Transactions: make([]Transaction, 0)}
+	return &PaymentSystem{Users: make(map[string]*User), Transactions: make([]Transaction, 0)}
 }
 
-func (ps *PaymentSystem) AddUser(name string, balance float64) {
-	user := NewUser(name, balance)
-	id := user.ID
+func (ps *PaymentSystem) AddUser(id string, name string, balance float64) {
+	user := NewUser(id, name, balance)
 	ps.Users[id] = user
 }
 
-func (ps *PaymentSystem) AddTransaction(id1 int, id2 int, amount float64) {
+func (ps *PaymentSystem) AddTransaction(id1 string, id2 string, amount float64) {
 	trx := Transaction{FromID: id1, ToID: id2, Amount: amount}
 	ps.Transactions = append(ps.Transactions, trx)
 }
@@ -91,16 +85,14 @@ func main() {
 
 	ps := NewPaymentSystem()
 
-	ps.AddUser("Khena", 400)
-	ps.AddUser("Shiro", 200)
+	ps.AddUser("1", "Khena", 400)
+	ps.AddUser("2", "Shiro", 200)
 
-	ps.AddTransaction(1, 2, 100)
-	ps.AddTransaction(1, 2, 91.23)
-	ps.AddTransaction(2, 1, 44.41)
-	ps.AddTransaction(2, 1, 250.12)
-	ps.AddTransaction(1, 2, 121.17)
-	
-	
+	ps.AddTransaction("1", "2", 100)
+	ps.AddTransaction("1", "2", 91.23)
+	ps.AddTransaction("2", "1", 44.41)
+	ps.AddTransaction("2", "1", 250.12)
+	ps.AddTransaction("1", "2", 121.17)
 
 	for idx := range ps.Transactions {
 		ps.ProcessingTransaction(idx)
