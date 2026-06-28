85533a341f9c9a0fa6dbddaa952b8efb598f1b28
Che-Misty <fash1210@gmail.com>
2026-06-29 00:51:11 +0300

    Исправление кода

--- main.go
+++ main.go
@@ -5,44 +5,38 @@ import (
 	"sync"
 )
 
-var uID int
-
 type User struct {
-	ID      int
+	ID      string
 	Name    string
 	Balance float64
 	mu      sync.Mutex
 }
 
-func NewUser(name string, balance float64) *User {
-	uID += 1
-
-	return &User{ID: uID, Name: name, Balance: balance}
+func NewUser(id string, name string, balance float64) *User {
+	return &User{ID: id, Name: name, Balance: balance}
 }
 
 type Transaction struct {
-	FromID int
-	ToID   int
+	FromID string
+	ToID   string
 	Amount float64
 }
 
 type PaymentSystem struct {
-	Users            map[int]*User
+	Users            map[string]*User
 	TransactionQueue []Transaction
-	mu               sync.Mutex
 }
 
 func NewPaymentSystem() *PaymentSystem {
-	return &PaymentSystem{Users: make(map[int]*User), TransactionQueue: make([]Transaction, 0)}
+	return &PaymentSystem{Users: make(map[string]*User), TransactionQueue: make([]Transaction, 0)}
 }
 
-func (ps *PaymentSystem) AddUser(name string, balance float64) {
-	user := NewUser(name, balance)
-	id := user.ID
+func (ps *PaymentSystem) AddUser(id string, name string, balance float64) {
+	user := NewUser(id, name, balance)
 	ps.Users[id] = user
 }
 
-func (ps *PaymentSystem) AddTransaction(id1 int, id2 int, amount float64) {
+func (ps *PaymentSystem) AddTransaction(id1 string, id2 string, amount float64) {
 	trx := Transaction{FromID: id1, ToID: id2, Amount: amount}
 	ps.TransactionQueue = append(ps.TransactionQueue, trx)
 }
@@ -53,31 +47,36 @@ func (u *User) Deposit(amount float64) {
 	u.Balance += amount
 }
 
-func (u *User) Withdraw(amount float64) (bool, error) {
+func (u *User) Withdraw(amount float64) error {
 	u.mu.Lock()
 	defer u.mu.Unlock()
 
 	if amount > u.Balance {
-		return false, fmt.Errorf("User %s haven't enought money", u.Name)
+		return fmt.Errorf("User %s haven't enought money", u.Name)
 	}
 	u.Balance -= amount
-	return true, nil
+	return nil
 }
 
 func (ps *PaymentSystem) ProcessingTransaction(trx Transaction) error {
-	sender := ps.Users[trx.FromID]
-	recipient := ps.Users[trx.ToID]
-
-	if len(sender.Name) == 0 {
-		return fmt.Errorf("User %s not found!", sender.Name)
+	sender, ok := ps.Users[trx.FromID]
+	if !ok || sender == nil {
+		return fmt.Errorf("User %s not found!", trx.FromID)
 	}
 
-	if len(recipient.Name) == 0 {
-		return fmt.Errorf("User %s not found!", recipient.Name)
+	recipient, ok := ps.Users[trx.ToID]
+	if !ok || recipient == nil {
+		return fmt.Errorf("User %s not found!", trx.ToID)
 	}
 
-	if ok, err := sender.Withdraw(trx.Amount); !ok {
+	if trx.Amount < 0 {
+		return fmt.Errorf("invalid amount: %.2f", trx.Amount)
+	}
+
+	err := sender.Withdraw(trx.Amount)
+	if err != nil {
 		return err
+
 	}
 	recipient.Deposit(trx.Amount)
 	return nil
@@ -96,31 +95,31 @@ func main() {
 
 	ps := NewPaymentSystem()
 
-	ps.AddUser("Khena", 400)
-	ps.AddUser("Shiro", 200)
+	ps.AddUser("1", "Khena", 400)
+	ps.AddUser("2", "Shiro", 200)
 
-	ps.AddTransaction(1, 2, 100)
-	ps.AddTransaction(1, 2, 91.23)
-	ps.AddTransaction(2, 1, 44.41)
-	ps.AddTransaction(2, 1, 250.12)
-	ps.AddTransaction(1, 2, 121.17)
-	ps.AddTransaction(1, 2, 500)
-	ps.AddTransaction(1, 2, 11.11)
-	ps.AddTransaction(1, 2, 10.01)
-	ps.AddTransaction(2, 1, 250.12)
-	ps.AddTransaction(2, 1, 54.12)
+	ps.AddTransaction("1", "2", 100)
+	ps.AddTransaction("1", "2", 91.23)
+	ps.AddTransaction("2", "1", -44.41)
+	ps.AddTransaction("2", "1", 250.12)
+	ps.AddTransaction("1", "2", 121.17)
+	ps.AddTransaction("1", "2", -500)
+	ps.AddTransaction("1", "2", 11.11)
+	ps.AddTransaction("1", "2", 10.01)
+	ps.AddTransaction("2", "1", 250.12)
+	ps.AddTransaction("2", "1", 54.12)
 
 	ch := make(chan Transaction, len(ps.TransactionQueue))
 
 	for _, t := range ps.TransactionQueue {
 		ch <- t
 	}
+	close(ch)
 
 	for i := 0; i < 3; i++ {
 		wg.Go(func() { ps.Worker(ch) })
 	}
 
-	close(ch)
 	wg.Wait()
 
 	for _, user := range ps.Users {
