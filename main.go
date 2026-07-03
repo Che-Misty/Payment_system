26f7c943a67b38b2ab3f5b2f7cad5ff867f03be6
Che-Misty <33163106+Che-Misty@users.noreply.github.com>
2026-07-03 07:06:40 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,31 +4,30 @@ import (
 	"fmt"
 )
 
-var uID int
-
 type User struct {
-	ID      int
+	ID      string
 	Name    string
-	Balance float32
+	Balance float64
 }
 
-func (u *User) Deposit(amount float32) {
+func (u *User) Deposit(amount float64) {
 	u.Balance += amount
 	fmt.Printf("Баланс пользователя %s равен: %.2f рублей.\n", u.Name, u.Balance)
 }
 
-func (u *User) Withdraw(amount float32) {
+func (u *User) Withdraw(amount float64) error {
 	if amount > u.Balance {
-		fmt.Printf("На счету пользователя %s недостаточно средств для списания %.2f рублей.\n", u.Name, u.Balance)
-		return
+		return fmt.Errorf("На счету пользователя %s недостаточно средств для списания %.2f рублей.\n", u.Name, amount)
+
 	}
 	u.Balance -= amount
 	fmt.Printf("Баланс пользователя %s равен: %.2f рублей.\n", u.Name, u.Balance)
+	return nil
 }
 
 func main() {
-	user1 := &User{1, "Kate", 100}
-	user2 := &User{2, "Dung", 100}
+	user1 := &User{"1", "Kate", 100}
+	user2 := &User{"2", "Dung", 100}
 
 	user2.Withdraw(150)
 	user1.Deposit(200)
